29755ab77f0d78a8e9811b94582b4701ea3008d3
unknown <ceasedinsanity.ct@gmail.com>
2022-01-15 00:18:51 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Explorables Plugin
+
+Create structures that generate custom maps when entered.
+
+## Example usage
+
+1. Create a structure using [the-travelers-plus](https://github.com/LightningWB/the-travelers-plus)
+
+2. Add an explorable blueprint to the structure so you can enter it with emit("explorables", "load", [STRUCTURE_ID], [BLUEPRINT_OBJ])
+
+```js
+const { emit, makePlugin } = global.plugins;
+
+const plugin = makePlugin("example");
+
+// Explorable Blueprint
+const blueprint = {
+  // Unique explorable name
+  type: "small_home",
+  // Width of explorable (max 98)
+  width: 5,
+  // Height of explorable (max 98)
+  height: 5,
+  // Array of structureIds used in explorable (an exit structure is required is you want to leave the explorable)
+  pallete: ["wood_block", "exit"],
+  // Layout of the explorable using indices from the pallete (-1 is an empty space)
+  layout: [
+    0,  0,  0,  0, 0,
+    0, -1, -1, -1, 0,
+    0, -1, -1, -1, 0,
+    0, -1, -1, -1, 0,
+    0,  0,  0,  0, 0
+  ],
+};
+
+plugin.on("ready", () => {
+    ////////////////////////
+   // the-travelers-plus //
+  ////////////////////////
+
+  // Create Game Item
+  emit("travelers", "addGameItem", "home", {
+    "name": "small_home",
+    "title": "small home",
+    "type": "build",
+    "weight": 100,
+    "icon": "h",
+    "desc": "a small home that can be entered",
+    "craft": true,
+    "craft_time": 2,
+    "craft_data": {
+        "wood_block": {
+            "count": 1,
+            "title": "wood block"
+        }
+    },
+    "build": true,
+    "build_desc": "when placed, you will be able to enter the home."
+  });
+
+  // Add structure properties to item
+  emit("travelers", "addStructureData", {
+    id: "home",
+    placingItem: "home",
+    char: "h",
+    breakTime: 2,
+    isBreakable: true
+  });
+
+  // OPTIONAL - Make it craftable
+  emit("travelers", "addCraftableItem", "home", 1);
+
+
+    /////////////////
+   // Explorables //
+  /////////////////
+
+  // Make the structure explorable
+  emit("explorables", "load", "home", blueprint);
+});
+
+```
---
+++ index.js
@@ -0,0 +1,229 @@
+const { emit, makePlugin, worldGen, chunks, patches } = require("./bullet");
+
+const plugin = makePlugin("explorables");
+const storage = plugin.getStorage();
+
+const blueprints = {};
+
+if (!storage.explorables)
+{
+  storage.explorables = [];
+}
+if (!storage.unused)
+{
+  storage.unused = [];
+}
+
+// TODO: Base off world gen border
+const worldEdge = 5000000;
+worldGen.patchGenerator("edgeDist - Math.abs(y) < 1)", "edgeDist-Math.abs(y)<1){bottomtile='&#8193'}else if(edgeDist-Math.abs(y)>-1000)");
+patches.addPatch("YOU.getCoordString",
+  `return (YOU.x > 0 ? "+" : "") + YOU.x + ", " + (YOU.y > 0 ? "+" : "") + YOU.y;`,
+  `if(YOU.y>=5000000){return "- -";}else{return (YOU.x > 0 ? "+" : "") + YOU.x + ", " + (YOU.y > 0 ? "+" : "") + YOU.y;}`,
+  false
+);
+
+async function generateExplorable(structure, blueprint)
+{
+  // Get id from packed array
+  let explorableId;
+  if (storage.unused.length === 0)
+  {
+    explorableId = storage.explorables.length;
+  }
+  else
+  {
+    explorableId = storage.unused.pop();
+  }
+
+  // Create explorable
+  const explorable = {
+    type: blueprint.type,
+    x: explorableId % 1000,
+    y: Math.floor(explorableId / 1000),
+    entranceX: 0,
+    entranceY: 0,
+    exitToX: structure.public.x,
+    exitToY: structure.public.y
+  };
+
+  // Place blueprint
+  const worldPositionX = (-worldEdge + (200 * explorable.x));
+  const worldPositionY = (worldEdge + (200 * explorable.y));
+  const chunkCoords = chunks.toChunkCoords(worldPositionX, worldPositionY);
+
+  if (!chunks.isChunkCoordsLoaded(chunkCoords.x, chunkCoords.y))
+  {
+    await chunks.loadChunk(chunkCoords.x, chunkCoords.y);
+  }
+
+  const chunk = chunks.getChunkFromChunkCoords(chunkCoords.x, chunkCoords.y);
+
+  for (let x = worldPositionX; x < worldPositionX + blueprint.width + 2; ++x)
+  {
+    for (let y = worldPositionY; y < worldPositionY + blueprint.height + 2; ++y)
+    {
+      if (x === worldPositionX || x === worldPositionX + blueprint.width + 1
+       || y === worldPositionY || y === worldPositionY + blueprint.height + 1)
+      {
+        chunk[x + "|" + y] = [{public:{},private:{structureId:"barrier"}}];
+      }
+    }
+  }
+
+  blueprint.layout.forEach((palleteIndex, index) => {
+    if (palleteIndex < 0) return;
+    const x = 1 + worldPositionX + (index % blueprint.width);
+    const y = worldPositionY + (blueprint.height - Math.floor(index / blueprint.width));
+    const structureId = blueprint.pallete[palleteIndex];
+    if (structureId === "exit")
+    {
+      explorable.entranceX = x;
+      explorable.entranceY = y;
+      chunk[x + '|' + y] = [{
+        public: {},
+        private:{
+          structureId: blueprint.pallete[palleteIndex],
+          explorableId
+        }
+      }];
+      return;
+    }
+    chunk[x + '|' + y] = [{
+      public: {},
+      private:{
+        structureId: blueprint.pallete[palleteIndex]
+      }
+    }];
+  });
+
+  // Save
+  storage.explorables[explorableId] = explorable;
+  plugin.setStorage(storage);
+
+  chunks.unLoadChunk(chunkCoords.x, chunkCoords.y);
+
+  return explorableId;
+}
+
+async function deleteExplorable(structure)
+{
+  const { explorableId } = structure.private;
+  const explorable = storage.explorables[explorableId];
+  console.log(explorable);
+  if (!explorable)return;
+  storage.explorables[explorableId] = null;
+  storage.unused.push(explorableId);
+  const worldPositionX = (-worldEdge + (200 * explorable.x));
+  const worldPositionY = (worldEdge + (200 * explorable.y));
+  const chunkCoords = chunks.toChunkCoords(worldPositionX, worldPositionY);
+
+  if (!chunks.isChunkCoordsLoaded(chunkCoords.x, chunkCoords.y))
+  {
+    await chunks.loadChunk(chunkCoords.x, chunkCoords.y);
+  }
+  const chunk = chunks.getChunkFromChunkCoords(chunkCoords.x, chunkCoords.y);
+  const blueprint = blueprints[explorable.type];
+
+  for (let x = worldPositionX; x < worldPositionX + blueprint.width + 2; ++x)
+  {
+    for (let y = worldPositionY; y < worldPositionY + blueprint.height + 2; ++y)
+    {
+      chunk[x + "|" + y] = undefined;
+    }
+  }
+
+  // save
+  plugin.setStorage(storage);
+}
+
+
+plugin.on("explorables::load", (structureId, blueprint) => {
+  if (!structureId || !blueprint)
+  {
+    console.error("explorables::load requires a 'structure' and 'blueprint'");
+    return;
+  }
+
+  blueprints[blueprint.type] = blueprint;
+
+  plugin.on(`travelers::structurePlaced::${structureId}`, (structure) => {
+    generateExplorable(structure, blueprint)
+    .then((id) => structure.private.explorableId = id);
+  }, 0);
+
+  plugin.on(`travelers::structureBroke::${structureId}`, (structure) => {
+    deleteExplorable(structure);
+  }, 0);
+}, 0);
+
+
+plugin.on("travelers::onPlayerStep", async (player, cancel) => {
+  if (player.cache.travelData.dir === "")
+    return;
+
+  const obj = chunks.getObject(player.public.x, player.public.y);
+  if (!obj)return;
+
+  const explorableId = obj.private.explorableId;
+  if (explorableId !== undefined)
+  {
+    const targetLoc = {x:0,y:0};
+    if (obj.private.structureId === "exit")
+    {
+      targetLoc.x = storage.explorables[explorableId].exitToX;
+      targetLoc.y = storage.explorables[explorableId].exitToY;
+      player.private.explorableId = undefined;
+    }
+    else
+    {
+      targetLoc.x = storage.explorables[explorableId].entranceX;
+      targetLoc.y = storage.explorables[explorableId].entranceY;
+      player.private.explorableId = explorableId;
+    }
+
+    const chunkTarget = chunks.toChunkCoords(targetLoc.x, targetLoc.y);
+    if (!chunks.isChunkCoordsLoaded(chunkTarget.x, chunkTarget.y))
+    {
+      await chunks.loadChunk(chunkTarget.x, chunkTarget.y);
+    }
+    player.public.x = targetLoc.x;
+    player.public.y = targetLoc.y;
+    player.addPropToQueue("x", "y");
+  
+    cancel.set(true);
+  }
+});
+
+
+// Disable teleporters inside explorables
+function disable(player) {
+  const { explorableId } = player.private;
+  if (typeof explorableId === "number") return false;
+}
+plugin.on('equip_actions::high_teleporter::north', disable, 10);
+	plugin.on('equip_actions::high_teleporter::east', disable, 10);
+	plugin.on('equip_actions::high_teleporter::south', disable, 10);
+	plugin.on('equip_actions::high_teleporter::west', disable, 10);
+	// low teleporter
+	plugin.on('equip_actions::low_teleporter::north', disable, 10);
+	plugin.on('equip_actions::low_teleporter::east', disable, 10);
+	plugin.on('equip_actions::low_teleporter::south', disable, 10);
+	plugin.on('equip_actions::low_teleporter::west', disable, 10);
+
+
+plugin.on("ready", () => {
+  // Structures
+  emit("travelers", "addStructureData", {
+    id: "exit",
+    placingItem: "exit",
+    char: "X"
+  });
+  
+  emit("travelers", "addStructureData", {
+    id: "barrier",
+    placingItem: "barrier",
+    char: "&nbsp",
+    walkOver: false
+  });
+})
